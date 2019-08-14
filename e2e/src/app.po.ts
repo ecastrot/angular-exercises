890541ec1ce0b5f1f89124f850f6bf88e9242e37
Edison Castro <ecastrot@psl.com.co>
2019-08-14 08:14:59 -0500

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('psl-root h1')).getText();
+  }
+}
